476a0277598ca3599f40335554b8f4e54fbd0921
Hukum Yadav <hukumyadav45@gmail.com>
2023-09-07 16:13:29 +0530

    assignment complete

---
+++ src/app/components/Emotion.js
@@ -0,0 +1,15 @@
+// components/ChildComponent.js
+
+import React from 'react';
+
+const Emotion = () => {
+  return (
+    <div className='emotion'>
+      {/* Your child component content here */}
+
+      <h1> HELLLLLO</h1>
+    </div>
+  );
+};
+
+export default Emotion;
